6b53c4f1b5236a87c5081f6366c790c008747399
horaguy <157380662+horaguy@users.noreply.github.com>
2024-08-19 21:47:01 +0900

    Rename Godot Mono to Dotnet in cask script

--- .github/scripts/generate-godot-casks.sh
+++ .github/scripts/generate-godot-casks.sh
@@ -192,15 +192,15 @@ cask "godot-dotnet@$version" do
   depends_on cask: "dotnet-sdk"
   $depends_on_macos
 
-  app "Godot_mono.app"
+  app "Godot_dotnet.app"
   # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-  shimscript = "#{staged_path}/godot-mono.wrapper.sh"
-  binary shimscript, target: "godot-mono"
+  shimscript = "#{staged_path}/godot-dotnet.wrapper.sh"
+  binary shimscript, target: "godot-dotnet"
 
   preflight do
     File.write shimscript, <<~EOS
       #!/bin/bash
-      '#{appdir}/Godot_mono.app/Contents/MacOS/Godot' "\$@"
+      '#{appdir}/Godot_dotnet.app/Contents/MacOS/Godot' "\$@"
     EOS
   end
 
