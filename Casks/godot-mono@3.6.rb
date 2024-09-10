9cc4cd312b5317910c8958340e4b138b04bb1560
GitHub Action <action@github.com>
2024-09-10 01:00:07 +0000

    Update Godot's Casks by GitHub Action

---
+++ Casks/godot-mono@3.6.rb
@@ -0,0 +1,39 @@
+cask "godot-mono@3.6" do
+  version "3.6"
+  sha256 "06af48d9aa79a1ce7ddb712561bfebfa0b21e3eef635081caaca329911311503"
+
+  url "https://github.com/godotengine/godot/releases/download/#{version}-stable/Godot_v#{version}-stable_mono_osx.universal.zip",
+      verified: "github.com/godotengine/godot/"
+  name "Godot Engine"
+  desc "C# scripting capable version of Godot game engine"
+  homepage "https://godotengine.org/"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)[._-]stable$/i)
+    strategy :github_latest
+  end
+
+  depends_on cask: "dotnet-sdk"
+  depends_on macos: ">= :sierra"
+
+  app "Godot_mono.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/godot-mono.wrapper.sh"
+  binary shimscript, target: "godot-mono"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/bash
+      '#{appdir}/Godot_mono.app/Contents/MacOS/Godot' "$@"
+    EOS
+  end
+
+  uninstall quit: "org.godotengine.godot"
+
+  zap trash: [
+    "~/Library/Application Support/Godot",
+    "~/Library/Caches/Godot",
+    "~/Library/Saved Application State/org.godotengine.godot.savedState",
+  ]
+end
